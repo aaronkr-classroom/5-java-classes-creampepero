4b561adcc36a4855dff4d6932f8014cea503d7e0
creampepero <pyy_0413@naver.com>
2024-04-02 16:57:22 +0900

    5주차 실습 끝!

---
+++ src/Cat.java
@@ -0,0 +1,19 @@
+
+public class Cat extends Animal{ // cat is-a animal
+	String color;
+	String breed;
+	
+	void eat() {
+		System.out.println(this.getName() + " 먹이를 먹다~ ");
+		
+	}
+	
+	void scratch() {
+		System.out.println(this.getName() + "발톱으로 할퀴다");
+		
+	}
+	
+	public void sound() { //메서드 오버라이딩 ( 다른 객체에서 덮어쓰기)
+		System.out.println(this.getName() + "야옹, 야옹, meow, meow~~");
+	}
+}
